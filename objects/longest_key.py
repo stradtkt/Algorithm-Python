2bc01986a25bc5ea07bebf56b2ae3fed5b656535
Kevin Stradtman <stradtkt@gmail.com>
2018-12-23 22:42:01 -0500

    Return longest key

---
+++ objects/longest_key.py
@@ -0,0 +1,7 @@
+class LongestKey(dict):
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if not longest or len(key) > len(longest):
+                longest = key
+        return longest
