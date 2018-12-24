764b92252bb2245cd6d3d45e473924817a65245d
Kevin Stradtman <stradtkt@gmail.com>
2018-12-23 23:19:07 -0500

    added ordered linear search

---
+++ search/ordered_linear_search.py
@@ -0,0 +1,10 @@
+def search(ordered_list, term):
+    ordered_list_size = len(ordered_list)
+    for i in range(ordered_list_size):
+        if term == ordered_list[i]:
+            return i
+        elif ordered_list[i] > term:
+            return None
+    return None
+
+    
